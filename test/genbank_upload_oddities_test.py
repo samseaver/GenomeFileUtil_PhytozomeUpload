1192e80adb0b0a1e8140b039aabd4b1d643ce73a
Jason Kirk Baumohl <jkbaumohl@188.112.3.128.in-addr.dhcp.lbl.gov>
2018-01-02 13:58:22 -0800

    Custom file with oddities.

---
+++ test/genbank_upload_oddities_test.py
@@ -0,0 +1,82 @@
+import unittest
+import time
+import os
+import shutil
+
+try:
+    from ConfigParser import ConfigParser  # py2
+except:
+    from configparser import ConfigParser  # py3
+
+from Workspace.WorkspaceClient import Workspace as workspaceService
+from GenomeFileUtil.GenomeFileUtilImpl import GenomeFileUtil
+from GenomeFileUtil.GenomeFileUtilServer import MethodContext
+from DataFileUtil.DataFileUtilClient import DataFileUtil
+from pprint import pprint
+
+
+class GenomeFileUtilTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        token = os.environ.get('KB_AUTH_TOKEN', None)
+        # WARNING: don't call any logging methods on the context object,
+        # it'll result in a NoneType error
+        cls.ctx = MethodContext(None)
+        cls.ctx.update({'token': token,
+                        'provenance': [
+                            {'service': 'GenomeFileUtil',
+                             'method': 'please_never_use_it_in_production',
+                             'method_params': []
+                             }],
+                        'authenticated': 1})
+        config_file = os.environ.get('KB_DEPLOYMENT_CONFIG', None)
+        cls.cfg = {}
+        config = ConfigParser()
+        config.read(config_file)
+        for nameval in config.items('GenomeFileUtil'):
+            cls.cfg[nameval[0]] = nameval[1]
+        cls.wsURL = cls.cfg['workspace-url']
+        cls.wsClient = workspaceService(cls.wsURL, token=token)
+        cls.serviceImpl = GenomeFileUtil(cls.cfg)
+        gbk_path = "data/Arabidopsis_gbff/Arab_Chloro_Modified.gbff"
+        ws_obj_name = 'ArabidopsisChloro'
+        suffix = int(time.time() * 1000)
+        cls.wsName = "test_GenomeFileUtil_" + str(suffix)
+        ret = cls.wsClient.create_workspace({'workspace': cls.wsName})
+        result = cls.serviceImpl.genbank_to_genome(
+            cls.ctx,
+            {
+              'file': {
+                  'path': gbk_path},
+              'workspace_name': cls.wsName,
+              'genome_name': ws_obj_name,
+              'generate_ids_if_needed': 1
+            })[0]
+        print("HERE IS THE RESULT:")
+        data_file_cli = DataFileUtil(os.environ['SDK_CALLBACK_URL'], 
+                                token=cls.ctx['token'],
+                                service_ver='dev')
+        cls.genome = data_file_cli.get_objects({'object_refs': [result['genome_ref']]})['data'][0]['data']
+        print("GENE 1: ")
+        pprint(cls.genome['features'][0])
+        pprint(result)
+
+
+
+    @classmethod
+    def tearDownClass(cls):
+        if hasattr(cls, 'wsName'):
+            cls.wsClient.delete_workspace({'workspace': cls.wsName})
+            print('Test workspace was deleted')
+
+#    def test_incorrect(self):
+#        self.assertTrue( 1 == 0, "1 ne 0")
+
+    def test_unknown_molecule(self):
+        genome = self.__class__.genome
+        if 'warnings' in genome:
+            for genome_warning in genome['warnings']:
+                print("WARNING")
+                print(str(genome_warning)) 
+                self.assertNotIn("Genome molecule_type Unknown is not expected for domain Bacteria.", genome_warning)
+
