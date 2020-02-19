fbc723c5c4d45533ec006f8ac4f3cab1a180f7f9
Jay R Bolton <jayrbolton@gmail.com>
2020-02-19 10:32:31 -0800

    Add special test runner script that makes it easier to run a specific class or function

---
+++ scripts/run_tests_within_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+script_dir=$(dirname "$(readlink -f "$0")")
+export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
+export KB_AUTH_TOKEN=`cat /kb/module/work/token`
+export PYTHONPATH=$script_dir/../lib:$PATH:$PYTHONPATH
+
+# Set TEST_PATH to run a specific test. Eg: TEST_PATH=test.core.update_taxon_assignments_test
+export TEST_PATH=test.core.update_taxon_assignments_test
+
+cd $script_dir/../test
+python -m nose --with-coverage --cover-package=GenomeFileUtil --cover-html --cover-html-dir=/kb/module/work/test_coverage --nocapture --nologcapture $TEST_PATH
+cp /kb/module/.coveragerc .
+cp .coverage /kb/module/work/
+mkdir -p /kb/module/work/kb/module/lib/
+cp -R /kb/module/lib/GenomeFileUtil/ /kb/module/work/kb/module/lib/P
