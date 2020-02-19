2dedf6c4e96ecf3ce5d71c273f0a9511ee45ee79
Jay R Bolton <jayrbolton@gmail.com>
2020-02-19 11:25:42 -0800

    Fix test path

--- scripts/run_tests_within_container.sh
+++ scripts/run_tests_within_container.sh
@@ -5,7 +5,7 @@ export KB_AUTH_TOKEN=`cat /kb/module/work/token`
 export PYTHONPATH=$script_dir/../lib:$PATH:$PYTHONPATH
 
 # Set TEST_PATH to run a specific test. Eg: TEST_PATH=test.core.update_taxon_assignments_test
-export TEST_PATH=test.core.update_taxon_assignments_test
+export TEST_PATH=.
 
 cd $script_dir/../test
 python -m nose --with-coverage --cover-package=GenomeFileUtil --cover-html --cover-html-dir=/kb/module/work/test_coverage --nocapture --nologcapture $TEST_PATH
